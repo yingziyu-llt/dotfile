87fb0b6d007ee4d49ad71aeaa2bafc8c28009c22
影子鱼 <linletian1@sina.com>
2024-05-08 11:29:19 +0800

    lazyvim

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,54 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "<F12>",
+      ":w<CR>:split<CR>:te time python %<CR>i",
+      { silent = true, noremap = true }
+    )
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp",
+  callback = function()
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "<F12>",
+      "<ESC>:w<CR>:split<CR>:te g++ % -o %<.run -lm -std=c++20 -Wall -Wextra -Wshadow -Dllt -fsanitize=address -g && time ./%:t:r.run<CR>i",
+      { silent = true, noremap = true }
+    )
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "<F5>",
+      ":w<CR>:split<CR>:te python -m pdb %<CR>i",
+      { silent = true, noremap = true }
+    )
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp",
+  callback = function()
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "<F5>",
+      "<ESC>:w<CR>:split<CR>:te g++ % -o %<.run -lm -std=c++20 -Wall -Wextra -Wshadow -Dllt -fsanitize=address -g && gdb %<.run<CR>i",
+      { silent = true, noremap = true }
+    )
+  end,
+})
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<F5>", "<CMD> term gedit % <CR>")
+vim.keymap.set("n", "<F2>", "<CMD> e %<.in <CR>")
+vim.keymap.set("n", "<F3>", "<CMD> e %<.out <CR>")
+vim.keymap.set("n", "<F4>", "<CMD> e %<.cpp <CR>")
